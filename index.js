3b04df0ea3a17c4003ee9f2ad2da8010c171cf0f
Airammg <airam.marting@gmail.com>
2023-02-02 15:56:22 +0000

    Start Game / Game Over added

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 let clicking = false;
 
+const Game = function () {
+    let timerId = setInterval(function () {
+        new Fruit()
+    }, 5000)
+
+    this.gameOver = function (){
+        clearInterval(timerId)
+    }
+}
 const Fruit = function (width, heigth) {
     self = this
     let fruitNest = [
@@ -19,8 +28,6 @@ const Fruit = function (width, heigth) {
         }
     ]
 
-    
-
     this.element = document.createElement('div')
     this.maxRandomNumber = function (max = 880) {
         return Math.floor(Math.random() * max)
@@ -28,9 +35,7 @@ const Fruit = function (width, heigth) {
     this.selectedFruit = fruitNest[this.maxRandomNumber(fruitNest.length)]
     this.element.setAttribute('class', this.selectedFruit.name)
     this.width = this.element.clientWidth
-    this.element.style.left = this.maxRandomNumber(900-this.selectedFruit.width) + 'px';
-    // this.element.style.width = width + 'px'
-    // this.element.style.heigth = heigth + 'px'
+    this.element.style.left = this.maxRandomNumber(900-this.selectedFruit.width) + 'px'
     this.dettectCollision = function (e) {
         if (clicking) {
             let canvas = e.target.parentNode
@@ -41,11 +46,6 @@ const Fruit = function (width, heigth) {
     this.canvas = document.getElementById('canvas')
     this.canvas.appendChild(this.element)
 }
-/*this.fruitFalling = function() {
-    this.style.top = 
-}*/
-
-
 
 canvas.onmousedown = function () {
     clicking = !clicking
@@ -58,17 +58,15 @@ let fruit1 = new Fruit()
 let fruit2 = new Fruit()
 let fruit3 = new Fruit()
 
+let partida = new Game()
+setTimeout(function(){
+    partida.gameOver()
+},20001)
+
+//partida.gameOver(timerId)
+
 console.log(fruit1.width)
 
 
-
-
-// canvas.onclick = function () {
-//     console.log('clickeado canvas')
-// }
-
-// setTimeout(function() {
-//     console.log(clicking)
-// },5000)
 
 
