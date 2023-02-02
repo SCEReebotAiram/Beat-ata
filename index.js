2234c5589d8bb9b29d706d50eb9f4e99fffc599f
Airammg <airam.marting@gmail.com>
2023-02-02 16:46:30 +0000

    Images as background working

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ let clicking = false;
 
 const Game = function () {
     let timerId = setInterval(function () {
-        new Fruit()
+        console.log(new Fruit())
+        
     }, 5000)
 
     this.gameOver = function (){
@@ -15,16 +16,19 @@ const Fruit = function (width, heigth) {
         {
             name: 'apple',
             width: 40,
-            heigth: 40
+            heigth: 40,
+            image: "url('./assets/apple.png')",
         }, {
             name: 'pear',
-            width: 20,
-            heigth: 20
+            width: 40,
+            heigth: 40,
+            image: "url('./assets/pear.png')",
 
         }, {
             name: 'peach',
-            width: 50,
-            heigth: 50
+            width: 40,
+            heigth: 40,
+            image: "url('./assets/peach.png')",
         }
     ]
 
@@ -35,6 +39,8 @@ const Fruit = function (width, heigth) {
     this.selectedFruit = fruitNest[this.maxRandomNumber(fruitNest.length)]
     this.element.setAttribute('class', this.selectedFruit.name)
     this.width = this.element.clientWidth
+    this.element.style.backgroundImage = this.selectedFruit.image
+    this.element.style.backgroundSize = 'contain'
     this.element.style.left = this.maxRandomNumber(900-this.selectedFruit.width) + 'px'
     this.dettectCollision = function (e) {
         if (clicking) {
@@ -54,18 +60,14 @@ canvas.onmouseup = function () {
     clicking = !clicking
 }
 
-let fruit1 = new Fruit()
-let fruit2 = new Fruit()
-let fruit3 = new Fruit()
 
 let partida = new Game()
 setTimeout(function(){
     partida.gameOver()
 },20001)
 
-//partida.gameOver(timerId)
 
-console.log(fruit1.width)
+
 
 
 
