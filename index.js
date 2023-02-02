d662f8ff1531d513be3b352ef87ecf301dc9c6a8
Airammg <airam.marting@gmail.com>
2023-02-02 11:46:01 +0000

    Items collisions done. Working on item creation

--- index.js
+++ index.js
@@ -1,34 +1,62 @@
-const canvas = document.getElementById('canvas')
-const player = document.getElementById('player')
-player.setAttribute('class', 'manzana')
-player.addEventListener('mouseover', function(){
-    console.log('mouseovering', clicking)
-})
-
-
 let clicking = false;
 
-const Fruit = function (positionX, positionY,width, heigth ){
-    this.positionX = positionX,
-    this.positionY = positionY,
-    this.width = width,
+const canvas = document.getElementById('canvas')
+const fruit = document.getElementById('apple')
+fruit.setAttribute('class', 'apple')
+fruit.addEventListener('mouseover', dettectCollition)
+const fruit2 = document.getElementById('pear')
+fruit2.setAttribute('class', 'pear')
+fruit2.addEventListener('mouseover', dettectCollition)
+
+function dettectCollition(e) {
+    if (clicking) {
+        let canvas = e.target.parentNode
+        // let elementToDelete = e.target
+        canvas.removeChild(e.target)
+    }
+}
+
+
+const Fruit = function (positionX, positionY, width, heigth) {
+    this.positionX = positionX
+    this.positionY = positionY
+    this.width = width
     this.heigth = heigth
-} 
-
-let manzana = new Fruit(250, 100, 20, 20);
-console.log(manzana)
-
-canvas.onclick = function(){
-    console.log('clickeado canvas')
+    this.generateFruit = function() {
+        let newElement = document.createElement('div')
+        newElement.setAttribute('class', 'peach')
+        newElement.addEventListener('mouseover', dettectCollition)
+        canvas.appendChild(newElement)
+        newElement.style.left = maxRandomNumber()+'px';
+    }
 }
 
-canvas.onmousedown = function(){
+function maxRandomNumber(max=880){
+    return Math.floor(Math.random()*max)
+}
+
+
+canvas.onmousedown = function () {
+    clicking = !clicking
+}
+canvas.onmouseup = function () {
     clicking = !clicking
 }
 
-canvas.onmouseup =  function () {
-    clicking = !clicking
-}
+let apple = new Fruit(250, 100, 20, 20);
+let pear = new Fruit(300, 400, 20, 20)
+apple.generateFruit()
 
+
+
+
+
+// canvas.onclick = function () {
+//     console.log('clickeado canvas')
+// }
+
+// setTimeout(function() {
+//     console.log(clicking)
+// },5000)
 
 
