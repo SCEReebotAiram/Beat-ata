d8d3aff28704d99faaab03233fcdf24a08e64b96
Airammg <airam.marting@gmail.com>
2023-02-03 15:58:18 +0000

    Background and start button

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 let clicking = false;
+const buttonStart = document.getElementById('start-game-btn')
+buttonStart.addEventListener('click', function () {
+    let canvas = document.getElementById('canvas')
+    let startScreen = document.getElementById('startScreen')
+    startScreen.style.display = 'none'
+    canvas.style.display = 'block'
+    console.log(canvas, startScreen)
+    let partida = new Game()
+    setTimeout(function () {
+        partida.gameOver()
+    }, 30000)
+
+})
+
 
 const Game = function () {
     let timerId = setInterval(function () {
         new Fruit()
-        
-    }, 5000)
 
-    this.gameOver = function (){
+    }, 1000)
+
+    this.gameOver = function () {
         clearInterval(timerId)
     }
 }
@@ -40,12 +54,12 @@ const Fruit = function (width, heigth) {
     this.width = this.element.clientWidth
     this.element.style.backgroundImage = this.selectedFruit.image
     this.element.style.backgroundSize = 'contain'
-    this.element.style.left = this.maxRandomNumber(900-this.selectedFruit.width) + 'px'
+    this.element.style.left = this.maxRandomNumber(900 - this.selectedFruit.width) + 'px'
     this.positionTop = 0
     this.addTopId = setInterval(function () {
         self.positionTop = self.positionTop + 1
         self.element.style.top = self.positionTop + 'px'
-    },1)
+    }, 1)
     this.element.style.top = self.positionTop + 'px'
     this.checkPositionFruit = setInterval(function (e) {
         if (parseInt(self.element.style.top.slice(0, -2)) > 600 && self.element.parentNode) {
@@ -53,7 +67,7 @@ const Fruit = function (width, heigth) {
             clearInterval(self.checkPositionFruit)
             self.element.parentNode.removeChild(self.element)
         }
-    },10)
+    }, 10)
     this.dettectCollision = function (e) {
         if (clicking) {
             let canvas = e.target.parentNode
@@ -73,10 +87,6 @@ canvas.onmouseup = function () {
 }
 
 
-let partida = new Game()
-setTimeout(function(){
-    partida.gameOver()
-},30000)
 
 
 
