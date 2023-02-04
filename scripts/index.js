1ea7122e9090e5becacd344a0bf7f95f6a71868e
Airammg <airam.marting@gmail.com>
2023-02-04 00:23:08 +0000

    Adjusting code

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,55 @@
-let soundCollision = new Audio('../assets/sounds/slashkut.mp3');
-let gameOverSound = new Audio('../assets/sounds/game-over.mp3');
-let fruitDissFCanvas = new Audio('../assets/sounds/box-crash.mp3');
-
-let clicking = false;
 const buttonStart = document.getElementById('start-game-btn')
+const soundCollision = new Audio('../assets/sounds/slashkut.mp3')
+const gameOverSound = new Audio('../assets/sounds/game-over.mp3')
+const fruitDissFCanvas = new Audio('../assets/sounds/box-crash.mp3')
+
+let scorePoints = document.getElementById('score')
+let clicking = false
+let generationSpeed = 1000
+
 buttonStart.addEventListener('click', function () {
     let canvas = document.getElementById('canvas')
     let startScreen = document.getElementById('startScreen')
     startScreen.style.display = 'none'
     canvas.style.display = 'block'
-    console.log(canvas, startScreen)
     let partida = new Game()
-    setTimeout(function () {
-        partida.gameOver()
-    }, 30000)
-
 })
 
 
 const Game = function () {
+    let timeSpan = document.getElementById('timeSpan')
+    this.gameTime = 10000
+    this.chronoTime = this.gameTime / 1000 //seconds 
+    let counter = { value: 0 }
     let startSound = new Audio('../assets/sounds/happy.mp3');
-    let timerId = setInterval(function () {
-        new Fruit()
-    }, 5000)
-    this.gameOver = function (){
-        clearInterval(timerId)
-    }
+    let self = this
+    this.startGame = setInterval(function () {
+        // testing to change generationSpeed with item sliced counter
+        new Fruit(counter)
+    }, generationSpeed)
+    this.counterChanger = setInterval(function () {
+        scorePoints.innerText = `Smashed piñatas: ${counter.value}`
+    }, 500)
+    this.startTime = setInterval(function () {
+        timeSpan.innerText = `${self.chronoTime--}`
+    }, 1000)
+    this.gameOver = setTimeout(function () {
+        clearInterval(this.startGame)
+        let highestInterval = 1000
+        for (let i = 1; i < highestInterval; i++) {
+            clearInterval(i)
+        }
+        gameOver = true
+        if (this.gameOver) {
+            let finishScreen = document.getElementById('finishScreen')
+            finishScreen.style.display = 'block'
+            canvas.style.display = 'none'
+        }
+    }, this.gameTime + 1000)
 }
-const Fruit = function (width, heigth) {
+
+
+const Fruit = function (counter) {
     let self = this
     let fruitNest = [
         {
@@ -35,13 +57,15 @@ const Fruit = function (width, heigth) {
             width: 40,
             heigth: 40,
             image: "url('./assets/images/apple.png')",
-        }, {
+        },
+        {
             name: 'pear',
             width: 40,
             heigth: 40,
             image: "url('./assets/images/pear.png')",
 
-        }, {
+        },
+        {
             name: 'peach',
             width: 40,
             heigth: 40,
@@ -57,19 +81,17 @@ const Fruit = function (width, heigth) {
     this.width = this.element.clientWidth
     this.element.style.backgroundImage = this.selectedFruit.image
     this.element.style.backgroundSize = 'contain'
-
-    this.element.style.left = this.maxRandomNumber(900 - this.selectedFruit.width) + 'px'
-    this.element.style.left = this.maxRandomNumber(900-this.selectedFruit.width) + 'px'
+    this.maxWidth = 900 - this.selectedFruit.width
+    this.element.style.left = this.maxRandomNumber(this.maxWidth) + 'px'
 
     this.positionTop = 0
     this.addTopId = setInterval(function () {
         self.positionTop = self.positionTop + 1
         self.element.style.top = self.positionTop + 'px'
-
     }, 1)
     this.element.style.top = self.positionTop + 'px'
-    this.checkPositionFruit = setInterval(function (e) {
-        if (parseInt(self.element.style.top.slice(0, -2)) > 600 && self.element.parentNode) {
+    this.checkPositionFruit = setInterval(function () {
+        if (parseInt(self.element.style.top.slice(0, -2)) > 560 && self.element.parentNode) {
             clearInterval(self.addTopId)
             clearInterval(self.checkPositionFruit)
             self.element.parentNode.removeChild(self.element)
@@ -81,31 +103,21 @@ const Fruit = function (width, heigth) {
             let canvas = e.target.parentNode
             canvas.removeChild(e.target)
             soundCollision.play()
+            console.log(parseInt(scorePoints))
+            counter.value++
+
         }
     }
     this.element.addEventListener('mouseover', this.dettectCollision)
     this.canvas = document.getElementById('canvas')
     this.canvas.appendChild(this.element)
 }
+Fruit.prototype = Object.create(Game)
+Fruit.prototype.constructor = Fruit
 
-canvas.onmousedown = function () {
+canvas.onmousedown = function (e) {
     clicking = !clicking
 }
 canvas.onmouseup = function () {
     clicking = !clicking
 }
-
-
-let partida = new Game()
-
-setTimeout(function(){
-    partida.gameOver()
-    gameOverSound.play()
-},30000)
-
-
-
-
-
-
-
