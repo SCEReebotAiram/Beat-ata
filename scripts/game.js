e28abe9b8793b407accb9799db2ca7537efb58a5
Airammg <airam.marting@gmail.com>
2023-02-10 12:46:01 +0000

    Started refactoring

--- scripts/game.js
+++ scripts/game.js
@@ -1,81 +1,86 @@
-const Game = function () {
-  let timeSpan = document.getElementById('timeSpan')
-  this.gameTime = 30000
-  this.goal = this.gameTime / 1000 + Math.ceil(this.gameTime / 1000 / 4)
-  this.chronoTime = this.gameTime / 1000 //seconds 
-  let counter = { value: 0 }
-  let self = this
-  startSound.play()
-  startSound.volume = 0.3
-  this.startGame = setInterval(function () {
-    new Item(counter)
-  }, generationSpeed)
-  this.generationSpeedUp = function () {
-    clearInterval(self.startGame)
-    let newSpeed = 0
-    if (self.gameTime > 30000) {
-      newSpeed = 1.1
-    } else {
-      newSpeed = 1.25
-    }
-    this.startGame = setInterval(function () {
-      new Item(counter)
-    }, generationSpeed /= newSpeed)
-  }
-  this.counterChanger = setInterval(function () {
-    scorePoints.innerText = `Smashed piñatas: ${counter.value} / ${self.goal}`
-    if (counter.value > 0 && self.chronoTime % 5 === 0) {
-      self.generationSpeedUp()
-    }
-  }, 550)
-  this.startTime = setInterval(function () {
-    timeSpan.innerText = `${self.chronoTime--}`
-  }, 1000)
-  this.killAllInterval = function () {
-    let highestInterval = 1000000
-    for (let i = 1; i < highestInterval; i++) {
-      clearInterval(i)
-    }
-  }
-  this.removeAllChilds = function (classname) {
-    let arr = document.querySelectorAll('[class*=' + classname + ']')
-    arr.forEach(el => el.parentNode.removeChild(el))
-  }
-  this.gameOver = setTimeout(function () {
-    clearInterval(this.startGame)
-    self.killAllInterval()
-    generationSpeed = 1000
-    newSpeed = 1
-    gameOver = true
-    if (this.gameOver) {
-      let message = `You smashed ${counter.value} piñatas.`
-      let secondMesagge;
-      if (counter.value >= self.goal) {
-        secondMesagge = 'Well done!'
-        startSound.pause()
-        wellDoneSound.play()
-        wellDoneSound.volume = 0.5
+class Game {
+  constructor() {
+    this.scorePoints = document.getElementById('score')
+    this.timeSpan = document.getElementById('timeSpan')
+    this.gameTime = 30000
+    this.goal = this.gameTime / 1000 + Math.ceil(this.gameTime / 1000 / 4)
+    this.chronoTime = this.gameTime / 1000 //seconds 
+    this.counter = 0
+    startSound.play()
+    startSound.volume = 0.3
+    this.generationSpeed = 1000
+    this.startGame = setInterval(() => {
+      new Item()
+      console.log('here',this.generationSpeed)
+    }, this.generationSpeed)
+    this.newSpeed = 1
+    this.generationSpeedUp = () => {
+      clearInterval(this.startGame)
+      if (this.gameTime > 30000) {
+        this.newSpeed = 1.1
       } else {
-        secondMesagge = 'Try harder!'
-        startSound.pause()
-        gameOverSound.play()
-        gameOverSound.volume = 0.4
+        this.newSpeed = 1.25
+      } 
+      this.startGame = setInterval(() => {
+        new Item()
+      }, this.generationSpeed /= this.newSpeed)
+    }
+    this.counterChanger = setInterval(() => {
+      this.scorePoints.innerText = `Smashed piñatas: ${this.counter} / ${this.goal}`
+      console.log(this.generationSpeed)
+      if (this.counter > 0 && this.chronoTime % 5 === 0) {
+        this.generationSpeedUp()
       }
-      FINISHSCREEN.innerHTML = `<div> ${message} ${secondMesagge}!</div>
+    }, 550)
+    this.startTime = setInterval( () => {
+      this.timeSpan.innerText = `${this.chronoTime--}`
+    }, 1000)
+    this.killAllInterval = () => {
+      let highestInterval = 1000000
+      for (let i = 1; i < highestInterval; i++) {
+        clearInterval(i)
+      }
+    }
+    this.removeAllChilds = (classname) => {
+      let arr = document.querySelectorAll('[class*=' + classname + ']')
+      arr.forEach(el => el.parentNode.removeChild(el))
+    }
+    this.gameOver = setTimeout(() => {
+      clearInterval(this.startGame)
+      this.killAllInterval()
+      this.generationSpeed = 1000
+      this.newSpeed = 1
+      gameOver = true
+      if (this.gameOver) {
+        let message = `You smashed ${this.counter} piñatas.`
+        let secondMesagge
+        if (this.counter >= this.goal) {
+          secondMesagge = 'Well done!'
+          startSound.pause()
+          wellDoneSound.play()
+          wellDoneSound.volume = 0.5
+        } else {
+          secondMesagge = 'Try harder!'
+          startSound.pause()
+          gameOverSound.play()
+          gameOverSound.volume = 0.4
+        }
+        FINISHSCREEN.innerHTML = `<div> ${message} ${secondMesagge}!</div>
             <span id='playAgainBtn'>Play again?</span>
             `
-      let restartBtn = document.getElementById('playAgainBtn')
-      restartBtn.onclick = function () {
-        self.removeAllChilds('pinata')
-        self.removeAllChilds('mousetrace')
-        clicking = false
-        FINISHSCREEN.style.display = 'none'
-        CANVAS.style.display = 'block'
-        let partida = new Game()
-      }
-      finishScreen.style.display = 'flex'
-      canvas.style.display = 'none'
+        let restartBtn = document.getElementById('playAgainBtn')
+        restartBtn.onclick = () => {
+          this.removeAllChilds('pinata')
+          this.removeAllChilds('mousetrace')
+          clicking = false
+          FINISHSCREEN.style.display = 'none'
+          CANVAS.style.display = 'block'
+          let partida = new Game()
+        }
+        finishScreen.style.display = 'flex'
+        canvas.style.display = 'none'
 
-    }
-  }, this.gameTime + 1500)
+      }
+    }, this.gameTime + 1500)
+  }
 }
--- scripts/pinata.js
+++ scripts/pinata.js
@@ -1,82 +1,80 @@
-const Item = function (counter) {
-
-  let self = this
-  let pinataBox = [
+class Item extends Game {
+  constructor(counter) {
+    super(counter)
+    let pinataBox = [
       {
-          name: 'pinata1',
-          width: 160,
-          heigth: 120,
-          image: "url('./assets/images/pinata1.png')",
+        name: 'pinata1',
+        width: 160,
+        heigth: 120,
+        image: "url('./assets/images/pinata1.png')",
       },
       {
-          name: 'pinata2',
-          width: 160,
-          heigth: 120,
-          image: "url('./assets/images/pinata2.png')",
-
+        name: 'pinata2',
+        width: 160,
+        heigth: 120,
+        image: "url('./assets/images/pinata2.png')",
       },
       {
-          name: 'pinata3',
-          width: 160,
-          heigth: 120,
-          image: "url('./assets/images/pinata3.png')",
+        name: 'pinata3',
+        width: 160,
+        heigth: 120,
+        image: "url('./assets/images/pinata3.png')",
       }
-  ]
-this.element = document.createElement('div')
-this.maxRandomNumber = function (max = 880) {
-  return Math.floor(Math.random() * max)
-}
-this.selectedPinata = pinataBox[this.maxRandomNumber(pinataBox.length)]
-this.element.setAttribute('class', self.selectedPinata.name)
-this.width = this.element.clientWidth
-this.element.style.backgroundImage = this.selectedPinata.image
-this.element.style.backgroundSize = 'contain'
-this.maxWidth = 895 - this.selectedPinata.width
-this.element.style.left = this.maxRandomNumber(this.maxWidth)+5 + 'px'
-let initialPosLeft = self.element.style.left.slice(0, -2)
-this.positionLeft = initialPosLeft
-this.positionTop = 0
-this.degrees = 0
-// this.direction = 1
-this.addRotation = function () {
-  self.degrees+= 0.5
-  self.element.style.transform = `rotate(${self.degrees}deg)`
-}
-this.addFall = function () {
-  self.positionTop++
-  self.element.style.top = self.positionTop + 'px'
-}
-// this.addLateralMovement = function () {
-//   self.positionLeft++
-//   self.element.style.left = self.positionLeft + 'px'
-// }
-this.startMovement = setInterval(function () {
-  self.addFall()
-  self.addRotation()
-  // self.addLateralMovement()
-
-}, 1)
-this.checkPositionPinata = setInterval(function (e) {
-  if (parseInt(self.element.style.top.slice(0, -2)) > 600-self.selectedPinata.heigth && self.element.parentNode || self.positionLeft > 850) {
-    clearInterval(self.startMovement)
-    clearInterval(self.checkPositionPinata)
-    self.element.parentNode.removeChild(self.element)
-    pinatatDissFCanvas.play()
-    pinatatDissFCanvas.volume = 0.4
+    ]
+    this.element = document.createElement('div')
+    this.maxRandomNumber =  (max = 880) => {
+      return Math.floor(Math.random() * max)
+    }
+    this.selectedPinata = pinataBox[this.maxRandomNumber(pinataBox.length)]
+    this.element.setAttribute('class', this.selectedPinata.name)
+    this.width = this.element.clientWidth
+    this.element.style.backgroundImage = this.selectedPinata.image
+    this.element.style.backgroundSize = 'contain'
+    this.maxWidth = 895 - this.selectedPinata.width
+    this.element.style.left = this.maxRandomNumber(this.maxWidth) + 5 + 'px'
+    this.initialPosLeft = this.element.style.left.slice(0, -2)
+    this.positionLeft = this.initialPosLeft
+    this.positionTop = 0
+    this.degrees = 0
+    // this.direction = 1
+    this.addRotation =  () => {
+      this.degrees += 0.5
+      this.element.style.transform = `rotate(${this.degrees}deg)`
+    }
+    this.addFall = () => {
+      this.positionTop++
+      this.element.style.top = this.positionTop + 'px'
+    }
+    // this.addLateralMovement =  () {
+    //   this.positionLeft++
+    //   this.element.style.left = this.positionLeft + 'px'
+    // }
+    this.startMovement = setInterval(() => {
+      this.addFall()
+      this.addRotation()
+      // this.addLateralMovement()
+    }, 1)
+    this.checkPositionPinata = setInterval((e) => {
+      if (parseInt(this.element.style.top.slice(0, -2)) > 600 - this.selectedPinata.heigth && this.element.parentNode || this.positionLeft > 850) {
+        clearInterval(this.startMovement)
+        clearInterval(this.checkPositionPinata)
+        this.element.parentNode.removeChild(this.element)
+        pinatatDissFCanvas.play()
+        pinatatDissFCanvas.volume = 0.4
+      }
+    }, 10)
+    this.dettectCollision = (e) => {
+      if (clicking) {
+        let parent = e.target.parentNode
+        parent.removeChild(e.target)
+        soundCollision.play()
+        soundCollision.volume = 0.4
+        this.counter++
+      }
+    }
+    this.element.addEventListener('mouseover', this.dettectCollision)
+    CANVAS.appendChild(this.element)
   }
-}, 10)
-this.dettectCollision = function (e) {
-  if (clicking) {
-    let parent = e.target.parentNode
-    parent.removeChild(e.target)
-    soundCollision.play()
-    soundCollision.volume = 0.4
-    counter.value++
-  }
-}
-this.element.addEventListener('mouseover', this.dettectCollision)
-  CANVAS.appendChild(this.element)
 }
 
-Item.prototype = Object.create(Game)
-Item.prototype.constructor = Item
+
